1038d9ca38a61b39131d1348b73922cbb1bff79c
Clarence Hairston <djfliq1@gmail.com>
2018-11-25 18:36:35 -0500

    'Easy'

--- Timer/TimerMain.py
+++ Timer/TimerMain.py
@@ -8,8 +8,12 @@ import subprocess
 
 def main():
     while True:
-        subprocess.call('git commit', shell=True)
+        subprocess.call("git add test.csv", shell=True)
+        time.sleep(1)
+        subprocess.call("git commit -m 'Easy'", shell=True)
+        time.sleep(1)
         subprocess.call('git push', shell=True)
+        time.sleep(1)
 
         set_time = 5
 
