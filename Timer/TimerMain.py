fd27e10729d768583b88d141d17951c66be43ff1
Clarence Hairston <djfliq1@gmail.com>
2018-11-26 14:13:54 -0500

    'AutomatedPushComplete'

--- Timer/TimerMain.py
+++ Timer/TimerMain.py
@@ -20,12 +20,30 @@ def main():
 #This is the countdown timer, in seconds. Here is where you set how often you want to update your repository
         set_time = 5
 
+
+        try:
+            # A test.csv file is used as the file to be updated.
+            with open("../test.csv", 'r+') as csv_file:
+
+                dataR = csv_file.read()
+                dataW = csv_file.writelines("")
+                numOfLines = len(dataR.splitlines())
+
+                if (numOfLines >= 20):
+                    return dataW
+                else:
+                    print("No reset needed at this time.\n")
+
+        # To break out of the loop, in your terminal you can just press ctrl+break
+        except:
+            print("File Reset function malfunctioned!")
+
         try:
             when_to_stop = abs(int(set_time))
 
 # A test.csv file is used as the file to be updated.
-            with open("../test.csv",'w') as csv_file:
-                #csv_reader = csv.reader(csv_file)
+            with open("../test.csv",'a') as csv_file:
+                csv_reader = csv.reader(csv_file)
                 csv_writer = csv.writer(csv_file, delimiter='\t')
 
                 csv_writer.writerow("Not So Random Test Quote")
@@ -35,6 +53,9 @@ def main():
 # To break out of the loop, in your terminal you can just press ctrl+break
         except:
             print("Your file/s were not updated!")
+
+
+
 # Below is the structure of the countdown timer and how it will display in your terminal
         while when_to_stop > 0:
             m, s = divmod(when_to_stop, 60)
