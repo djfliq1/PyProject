bdec611485081814cdfc27ad1a774d8150cff759
Clarence Hairston <djfliq1@gmail.com>
2018-12-11 14:15:55 -0500

    'AutomatedPushComplete'

--- Timer/roboPiper.py
+++ Timer/roboPiper.py
@@ -1,5 +1,11 @@
-import subprocess
-import csv
+import hashlib
 
 
+filename = '../README.md'
+hasher = hashlib.md5()
+with open(filename,'rb') as open_file:
+    content = open_file.read()
+    hasher.update(content)
+print(hasher.hexdigest())
 
+#728a437926785e3bea25a23f1dc3edc3
